578e9c41e6174227c759f250ce8491344df39065
Julien Cohen-Adad <jcohen@polymtl.ca>
2023-01-30 18:39:16 -0500

    Fixed num workers
    Fixes https://github.com/jcohenadad/monai-tutorial/issues/1

--- spleen_segmentation_3d.py
+++ spleen_segmentation_3d.py
@@ -128,16 +128,16 @@ plt.imshow(label[:, :, 80])
 plt.show()
 
 # Define CacheDataset and DataLoader for training and validation
-train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=4)
+train_ds = CacheDataset(data=train_files, transform=train_transforms, cache_rate=1.0, num_workers=2)
 # train_ds = Dataset(data=train_files, transform=train_transforms)
 
 # use batch_size=2 to load images and use RandCropByPosNegLabeld
 # to generate 2 x 4 images for network training
-train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=4)
+train_loader = DataLoader(train_ds, batch_size=2, shuffle=True, num_workers=2)
 
-val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=4)
+val_ds = CacheDataset(data=val_files, transform=val_transforms, cache_rate=1.0, num_workers=2)
 # val_ds = Dataset(data=val_files, transform=val_transforms)
-val_loader = DataLoader(val_ds, batch_size=1, num_workers=4)
+val_loader = DataLoader(val_ds, batch_size=1, num_workers=2)
 
 # Create Model, Loss, Optimizer
 # standard PyTorch program style: create UNet, DiceLoss and Adam optimizer
