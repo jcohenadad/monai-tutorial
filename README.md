bfd5bd72ba6c083a41cec9c09cb5200e52302b48
jcohenadad <jcohen@polymtl.ca>
2023-01-30 17:12:47 -0500

    Clarified deps install

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # monai-tutorial
+
+Before running the tutorials, [install all dependencies](https://docs.monai.io/en/latest/installation.html#installing-the-recommended-dependencies).  
---
+++ spleen_segmentation_3d.py
@@ -0,0 +1,37 @@
+"""
+MONAI tutorial for 3D segmentation.
+https://github.com/Project-MONAI/tutorials/blob/main/3d_segmentation/spleen_segmentation_3d.ipynb
+"""
+
+from monai.utils import first, set_determinism
+from monai.transforms import (
+    AsDiscrete,
+    AsDiscreted,
+    EnsureChannelFirstd,
+    Compose,
+    CropForegroundd,
+    LoadImaged,
+    Orientationd,
+    RandCropByPosNegLabeld,
+    SaveImaged,
+    ScaleIntensityRanged,
+    Spacingd,
+    Invertd,
+)
+from monai.handlers.utils import from_engine
+from monai.networks.nets import UNet
+from monai.networks.layers import Norm
+from monai.metrics import DiceMetric
+from monai.losses import DiceLoss
+from monai.inferers import sliding_window_inference
+from monai.data import CacheDataset, DataLoader, Dataset, decollate_batch
+from monai.config import print_config
+from monai.apps import download_and_extract
+import torch
+import matplotlib.pyplot as plt
+import tempfile
+import shutil
+import os
+import glob
+
+print_config()
